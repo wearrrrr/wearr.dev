545fd87c698a60f1a2345f55bd32a76a7c587c00
wearrrrr <rowancole19@gmail.com>
2023-05-24 21:18:45 -0500

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,11 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
-        contact: resolve(__dirname, 'contact.html'),
-        projects: resolve(__dirname, 'projects.html'),
-        files: resolve(__dirname, 'files.html'),
-        hire: resolve(__dirname, 'hire-me.html'),
+        main: resolve(__dirname, '/pages/index.html'),
+        contact: resolve(__dirname, '/pages/index.html'),
+        projects: resolve(__dirname, '/pages/projects.html'),
+        files: resolve(__dirname, '/pages/files.html'),
+        hire: resolve(__dirname, '/pages/hire-me.html'),
       },
     },
   },
