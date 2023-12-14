f7f171fa979d559923a554a56b357f0c6e03340d
wearrrrr <rowancole19@gmail.com>
2023-12-14 13:29:08 -0600

    More UI changes and add MW affiliation

--- public/main.js
+++ public/main.js
@@ -1,11 +1,19 @@
-const textWrapper = document.querySelector('.ml9 .letters');
-textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
-anime.timeline({loop: false})
-.add({
-  targets: '.letter',
-  scale: [0, 1],
-  easing: 'spring(1, 90, 10, 0)',
-  duration: 2500,
-  delay: (el, i) => 45 * (i+1)
-})
+
+function timelineText() {
+  let ml9 = document.querySelector('.ml9');
+  ml9.style.display = "inline-block"
+  let textWrapper = document.querySelector('.ml9 .letters');
+  textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+  
+  anime.timeline({loop: false})
+  .add({
+    targets: '.letter',
+    scale: [0, 1],
+    easing: 'spring(1, 90, 10, 0)',
+    duration: 2500,
+    delay: (el, i) => 45 * (i+1)
+  })
+}
+
+document.addEventListener('DOMContentLoaded', timelineText, false);
