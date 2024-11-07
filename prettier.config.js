7f79001a34daa36e4f448e384ea5b417d2e1584c
wearrrrr <contact@wearr.dev>
2024-11-07 15:04:05 -0600

    Integrate prettier, add linting, and add a ton of content to the website itself.

---
+++ prettier.config.js
@@ -0,0 +1,14 @@
+/** @type {import("prettier").Config} */
+const config = {
+    tabWidth: 4,
+    semi: true,
+    trailingComma: "es5",
+    bracketSpacing: true,
+    bracketSameLine: true,
+    arrowParens: "always",
+    plugins: ["prettier-plugin-astro"],
+    printWidth: 200,
+    singleQuote: false,
+};
+
+export default config;
